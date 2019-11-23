b7f880c6241d11268318b994115185244f7b64f0
JasonFairchild <ammonjasonfairchild@gmail.com>
2019-11-22 17:10:11 -0700

    Initial project creation

---
+++ src/main/java/Excercises.java
@@ -0,0 +1,18 @@
+
+
+public class Excercises {
+    static String[] Permutations(String s){
+        s.toCharArray();
+    }
+
+    static String subPermutation(String s){
+        if(s.length() < 2) {
+            //stop
+            //return string
+        }else{
+            //go through remaining characters
+            //call subPerm
+            //build string
+        }
+    }
+}
